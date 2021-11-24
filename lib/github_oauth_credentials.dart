c9a31f88d24c37b337dc3b09516dd3887fa2141b
Darwin <5746693+darwin67@users.noreply.github.com>
2021-11-24 00:10:04 -0800

    Github OAuth (#1)
    * Add http and oauth2 packages
    * Add url launcher
    * Add github oauth authn
    * Setup version
    * Add Makefile
    
    Co-authored-by: Darwin D Wu <darwin67@users.noreply.github.com>

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,5 @@
+const githubClientId = "d73aee05b833d6514163";
+const githubClientSecret = "4f45ba4006b5c43cb4c66cd08b6e471d700e1279";
+
+// OAuth scopes for repository and user information
+const githubScopes = ["repo", "read:org"];
